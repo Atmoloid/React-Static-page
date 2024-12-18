6e0ed12e2b5a49625334d7aab14d88125225096e
Lorenzo Pica <157609695+Atmoloid@users.noreply.github.com>
2024-12-18 11:22:24 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import { createRoot }  from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+    <>
+    <img src="react-logo.png" width="40px"/>
+    <h1>Fun facts about React</h1>
+    <ul/>
+    <li>Was first released in 2013</li>
+    <li>Created by Jordan Walke</li>
+    <li>Over 100k stars on Github</li>
+    <li>Is mantained by Meta</li>
+    <li>Powers thousand of enterprise apps, including mobile apps</li>
+    
+    </>
+)
